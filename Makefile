8a737d266935b9d0ecb59b0c5af1ae301978e68c
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2025-12-07 16:47:46 +0000

    added func to create db header and validate

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ TARGET = bin/dbview
 SRC = $(wildcard src/*.c)
 OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
 
-run: clean default
-	./$(TARGET) -f ./mynewdb.db 
-	./$(TARGET) -f ./mynewdb.db -n 
+run: clean default 
+	./$(TARGET) -f ./mynewdb.db -n
+	./$(TARGET) -f ./mynewdb.db
 	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
 
 default: $(TARGET)
--- src/main.c
+++ src/main.c
@@ -18,7 +18,10 @@ int main(int argc, char *argv[]) {
     char *filepath = NULL;
     bool newfile = false;
     int c = 0;
+
     int dbfd = -1;
+    struct dbheader_t *dbhdr = NULL;
+    struct employee_t *employees = NULL;
 
     while ((c = getopt(argc, argv, "nf:")) != -1) {
         switch (c) {
@@ -48,16 +51,25 @@ int main(int argc, char *argv[]) {
             printf("Unable to create database file\n");
             return -1;
         }
+
+        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+            printf("Failed to create database header\n");
+            return -1;
+        }
     } else {
         dbfd = open_db_file(filepath);
         if (dbfd == STATUS_ERROR) {
             printf("Unable to open database file\n");
             return -1;
         }
+
+        if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+            printf("Failed to validate database header\n");
+            return -1;
+        }
     }
 
-    printf("Newfile: %d\n", newfile);
-    printf("Filepath: %s\n", filepath);
+    output_file(dbfd, dbhdr, employees);
 
     return 0;
 }
